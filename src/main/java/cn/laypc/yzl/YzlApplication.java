a7ce4470be12d4f1e7c0615d9dd4b0033a0ce5da
YoungE <405850873@qq.com>
2020-07-15 17:22:43 +0800

    初始化

---
+++ src/main/java/cn/laypc/yzl/YzlApplication.java
@@ -0,0 +1,13 @@
+package cn.laypc.yzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/laypc/yzl/YzlApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.laypc.yzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

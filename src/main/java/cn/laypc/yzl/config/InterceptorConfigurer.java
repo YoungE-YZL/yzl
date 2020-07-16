a222a34dba6b48d42cbcbe5b0640842bb2924eaa
YoungE <405850873@qq.com>
2020-07-16 15:22:47 +0800

    上传登录和注册页面

---
+++ src/main/java/cn/laypc/yzl/config/InterceptorConfigurer.java
@@ -0,0 +1,42 @@
+package cn.laypc.yzl.config;
+
+
+import cn.laypc.yzl.interceptor.AuthorityInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SpringBoot配置拦截器
+ * @time 2020/2/5 11:50
+ */
+@Configuration
+public class InterceptorConfigurer implements WebMvcConfigurer{
+	@Autowired
+	private AuthorityInterceptor loginInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		InterceptorRegistration addInterceptor =
+				registry.addInterceptor(loginInterceptor);
+		List<String> patterns = new ArrayList<>();
+
+		patterns.add("/css/**");
+		patterns.add("/img/**");
+		patterns.add("/js/**");
+		patterns.add("/layui/**/**");
+
+		patterns.add("/login.html");
+		patterns.add("/reg.html");
+		addInterceptor.addPathPatterns("/**").excludePathPatterns(patterns);
+	}
+
+
+
+}
+
---
+++ src/main/java/cn/laypc/yzl/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,37 @@
+package cn.laypc.yzl.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录拦截器
+ * @time 2020/2/5 11:50
+ */
+@Component
+public class AuthorityInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		//获取Httpsession对象
+		String url = request.getRequestURL().toString();
+		url = url.replace("//","");
+		url = url.substring(url.indexOf('/')+1);
+		HttpSession session = request.getSession();
+		//判断session中是否有登录信息
+		if(session.getAttribute("uid") == null) {
+			//没有登录信息,则重定向到登录页
+			response.sendRedirect("/login.html");
+			//执行拦截
+			return false;
+		}
+
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+}
